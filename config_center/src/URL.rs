6aa0a37a3ea948e4389792af5c6a328a3a095bd9
Zonglei Dong <dongzonglei@apache.org>
2022-07-10 22:27:40 +0800

    Feat(config_center): add config center module.

---
+++ config_center/src/URL.rs
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::collections::HashMap;
+
+pub struct URL {
+    parameters: HashMap<String, String>,
+}
+
+impl URL {
+
+    pub fn get_parameter(&self, key: String, defaultValue: String) -> String {
+        let value = match self.parameters.get(key.as_str()) {
+            Some(value) => value.clone(),
+            None => {
+                defaultValue.clone()
+            },
+        };
+        if value.is_empty() {
+            return defaultValue;
+        }
+        value
+    }
+}
---
+++ config_center/src/dynamic_configuration.rs
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::collections::HashMap;
+use crate::configuration_listener::ConfigurationListener;
+
+pub trait DynamicConfiguration {
+
+    fn add_listener(key: String, listener: impl ConfigurationListener);
+
+    fn remove_listener(key: String, listener: impl ConfigurationListener);
+
+    // TODO how to override
+
+    fn get_config(key: String, group: String, timeout: i32) -> String;
+
+    fn get_properties(key: String, group: String, timeout: i32) -> String;
+
+    fn publish_config(key: String, group: String, content: String) -> bool;
+
+    fn get_config_keys(group: String) -> HashMap<String, String>;
+}
