9fa46417fb9c57aee7c872fc5abdb5581745889c
yangyang <962032265@qq.com>
2022-08-20 23:02:59 +0800

    feat(dubbo-build): dubbo build for generating protoc codes

---
+++ dubbo-build/src/client.rs
@@ -0,0 +1,229 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use super::{generate_doc_comments, naive_snake_case, Attributes};
+use crate::{Method, Service};
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
+
+pub const CODEC_PATH: &str = "";
+
+/// Generate service for client.
+///
+/// This takes some `Service` and will generate a `TokenStream` that contains
+/// a public module with the generated client.
+pub fn generate<T: Service>(
+    service: &T,
+    emit_package: bool,
+    proto_path: &str,
+    compile_well_known_types: bool,
+    attributes: &Attributes,
+) -> TokenStream {
+    let service_ident = quote::format_ident!("{}Client", service.name());
+    let client_mod = quote::format_ident!("{}_client", naive_snake_case(service.name()));
+    let methods = generate_methods(service, emit_package, proto_path, compile_well_known_types);
+
+    let service_doc = generate_doc_comments(service.comment());
+
+    let package = if emit_package { service.package() } else { "" };
+    let path = format!(
+        "{}{}{}",
+        package,
+        if package.is_empty() { "" } else { "." },
+        service.identifier()
+    );
+
+    let mod_attributes = attributes.for_mod(package);
+    let struct_attributes = attributes.for_struct(&path);
+
+    quote! {
+        /// Generated client implementations.
+        #(#mod_attributes)*
+        pub mod #client_mod {
+            #![allow(
+                unused_variables,
+                dead_code,
+                missing_docs,
+                // will trigger if compression is disabled
+                clippy::let_unit_value,
+            )]
+            use triple::client::TripleClient;
+            use triple::codec::serde_codec::SerdeCodec;
+            use triple::invocation::*;
+            use triple::server::Decoding;
+
+            #service_doc
+            #(#struct_attributes)*
+            #[derive(Debug, Clone)]
+            pub struct #service_ident {
+                inner: TripleClient,
+                uri: String,
+            }
+
+            pub fn new() -> Self {
+                Self {
+                    inner: TripleClient::new(),
+                    uri: "".to_string(),
+                }
+            }
+
+            pub fn with_uri(mut self, uri: String) -> Self {
+                self.uri = uri.clone();
+                self.inner = self.inner.with_host(uri);
+                self
+            }
+
+            #methods
+
+        }
+    }
+}
+
+fn generate_methods<T: Service>(
+    service: &T,
+    emit_package: bool,
+    proto_path: &str,
+    compile_well_known_types: bool,
+) -> TokenStream {
+    let mut stream = TokenStream::new();
+    let package = if emit_package { service.package() } else { "" };
+
+    for method in service.methods() {
+        let path = format!(
+            "/{}{}{}/{}",
+            package,
+            if package.is_empty() { "" } else { "." },
+            service.identifier(),
+            method.identifier()
+        );
+
+        stream.extend(generate_doc_comments(method.comment()));
+
+        let method = match (method.client_streaming(), method.server_streaming()) {
+            (false, false) => generate_unary(&method, proto_path, compile_well_known_types, path),
+            (false, true) => {
+                generate_server_streaming(&method, proto_path, compile_well_known_types, path)
+            }
+            (true, false) => {
+                generate_client_streaming(&method, proto_path, compile_well_known_types, path)
+            }
+            (true, true) => generate_streaming(&method, proto_path, compile_well_known_types, path),
+        };
+
+        stream.extend(method);
+    }
+
+    stream
+}
+
+fn generate_unary<T: Method>(
+    method: &T,
+    proto_path: &str,
+    compile_well_known_types: bool,
+    path: String,
+) -> TokenStream {
+    let codec_name = syn::parse_str::<syn::Path>(CODEC_PATH).unwrap();
+    let ident = format_ident!("{}", method.name());
+    let (request, response) = method.request_response_name(proto_path, compile_well_known_types);
+
+    quote! {
+        pub async fn #ident(
+            &mut self,
+            request: Request<#request>,
+        ) -> Result<Response<#response>, triple::status::Status> {
+           let codec = #codec_name::default();
+           let path = http::uri::PathAndQuery::from_static(#path);
+           self.inner
+            .unary(
+                req,
+                codec,
+                path,
+            )
+            .await
+        }
+    }
+}
+
+fn generate_server_streaming<T: Method>(
+    method: &T,
+    proto_path: &str,
+    compile_well_known_types: bool,
+    path: String,
+) -> TokenStream {
+    let codec_name = syn::parse_str::<syn::Path>(CODEC_PATH).unwrap();
+    let ident = format_ident!("{}", method.name());
+
+    let (request, response) = method.request_response_name(proto_path, compile_well_known_types);
+
+    quote! {
+        pub async fn #ident(
+            &mut self,
+            request: Request<#request>,
+        ) -> Result<Response<Decoding<#response>>, triple::status::Status> {
+
+            let codec = #codec_name::default();
+            let path = http::uri::PathAndQuery::from_static(#path);
+            self.inner.server_streaming(request, codec, path).await
+        }
+    }
+}
+
+fn generate_client_streaming<T: Method>(
+    method: &T,
+    proto_path: &str,
+    compile_well_known_types: bool,
+    path: String,
+) -> TokenStream {
+    let codec_name = syn::parse_str::<syn::Path>(CODEC_PATH).unwrap();
+    let ident = format_ident!("{}", method.name());
+
+    let (request, response) = method.request_response_name(proto_path, compile_well_known_types);
+
+    quote! {
+        pub async fn #ident(
+            &mut self,
+            request: impl IntoStreamingRequest<Message = #request>
+        ) -> Result<Response<#response>, triple::status::Status> {
+            let codec = #codec_name::default();
+            let path = http::uri::PathAndQuery::from_static(#path);
+            self.inner.client_streaming(request, codec, path).await
+        }
+    }
+}
+
+fn generate_streaming<T: Method>(
+    method: &T,
+    proto_path: &str,
+    compile_well_known_types: bool,
+    path: String,
+) -> TokenStream {
+    let codec_name = syn::parse_str::<syn::Path>(CODEC_PATH).unwrap();
+    let ident = format_ident!("{}", method.name());
+
+    let (request, response) = method.request_response_name(proto_path, compile_well_known_types);
+
+    quote! {
+        pub async fn #ident(
+            &mut self,
+            request: impl IntoStreamingRequest<Message = #request>
+        ) -> Result<Response<Decoding<#response>>, triple::status::Status> {
+            let codec = #codec_name::default();
+            let path = http::uri::PathAndQuery::from_static(#path);
+            self.inner.streaming(request, codec, path).await
+        }
+    }
+}
---
+++ dubbo-build/src/prost.rs
@@ -0,0 +1,378 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use proc_macro2::TokenStream;
+use prost_build::{Config, Method, ServiceGenerator};
+use quote::ToTokens;
+use std::path::{Path, PathBuf};
+
+use crate::client;
+use crate::server;
+use crate::Attributes;
+
+/// Simple `.proto` compiling. Use [`configure`] instead if you need more options.
+///
+/// The include directory will be the parent folder of the specified path.
+/// The package name will be the filename without the extension.
+pub fn compile_protos(proto: impl AsRef<Path>) -> std::io::Result<()> {
+    let proto_path: &Path = proto.as_ref();
+
+    // directory the main .proto file resides in
+    let proto_dir = proto_path
+        .parent()
+        .expect("proto file should reside in a directory");
+
+    self::configure().compile(&[proto_path], &[proto_dir])?;
+
+    Ok(())
+}
+
+pub fn configure() -> Builder {
+    Builder {
+        build_client: true,
+        build_server: true,
+        proto_path: "super".to_string(),
+        protoc_args: Vec::new(),
+        compile_well_known_types: false,
+        include_file: None,
+        output_dir: None,
+        server_attributes: Attributes::default(),
+        client_attributes: Attributes::default(),
+    }
+}
+
+pub struct Builder {
+    build_client: bool,
+    build_server: bool,
+    proto_path: String,
+    compile_well_known_types: bool,
+    protoc_args: Vec<String>,
+    include_file: Option<PathBuf>,
+    output_dir: Option<PathBuf>,
+    server_attributes: Attributes,
+    client_attributes: Attributes,
+}
+
+impl Builder {
+    pub fn output_dir(mut self, output_dir: PathBuf) -> Self {
+        self.output_dir = Some(output_dir);
+        self
+    }
+
+    pub fn compile(
+        self,
+        protos: &[impl AsRef<Path>],
+        includes: &[impl AsRef<Path>],
+    ) -> std::io::Result<()> {
+        self.compile_with_config(Config::new(), protos, includes)
+    }
+
+    pub fn compile_with_config(
+        self,
+        mut config: Config,
+        protos: &[impl AsRef<Path>],
+        includes: &[impl AsRef<Path>],
+    ) -> std::io::Result<()> {
+        let out_dir = if let Some(out_dir) = self.output_dir.as_ref() {
+            out_dir.clone()
+        } else {
+            PathBuf::from(std::env::var("OUT_DIR").unwrap())
+        };
+        config.out_dir(out_dir);
+
+        if self.compile_well_known_types {
+            config.compile_well_known_types();
+        }
+
+        if let Some(path) = self.include_file.as_ref() {
+            config.include_file(path);
+        }
+
+        for arg in self.protoc_args.iter() {
+            config.protoc_arg(arg);
+        }
+
+        config.service_generator(Box::new(SvcGenerator::new(self)));
+        config.compile_protos(protos, includes)?;
+
+        Ok(())
+    }
+}
+
+pub struct SvcGenerator {
+    builder: Builder,
+    clients: TokenStream,
+    servers: TokenStream,
+}
+
+impl SvcGenerator {
+    fn new(builder: Builder) -> Self {
+        SvcGenerator {
+            builder,
+            clients: TokenStream::new(),
+            servers: TokenStream::new(),
+        }
+    }
+}
+
+impl ServiceGenerator for SvcGenerator {
+    fn generate(&mut self, service: prost_build::Service, _buf: &mut String) {
+        let svc = DubboService::new(service);
+        if self.builder.build_server {
+            let server = server::generate(
+                &svc,
+                true,
+                &self.builder.proto_path,
+                true,
+                &self.builder.server_attributes,
+            );
+            self.servers.extend(server);
+        }
+
+        if self.builder.build_client {
+            let client = client::generate(
+                &svc,
+                true,
+                &self.builder.proto_path,
+                true,
+                &self.builder.client_attributes,
+            );
+            self.clients.extend(client);
+        }
+    }
+
+    fn finalize(&mut self, buf: &mut String) {
+        if self.builder.build_client && !self.clients.is_empty() {
+            let clients = &self.clients;
+
+            let client_services = quote::quote! {
+                #clients
+            };
+
+            let ast: syn::File = syn::parse2(client_services).expect("invalid tokenstream");
+            let code = prettyplease::unparse(&ast);
+            buf.push_str(&code);
+
+            self.clients = TokenStream::default();
+        }
+
+        if self.builder.build_server && !self.servers.is_empty() {
+            let servers = &self.servers;
+
+            let server_services = quote::quote! {
+                #servers
+            };
+
+            let ast: syn::File = syn::parse2(server_services).expect("invalid tokenstream");
+            let code = prettyplease::unparse(&ast);
+            buf.push_str(&code);
+
+            self.servers = TokenStream::default();
+        }
+    }
+}
+
+pub struct DubboService {
+    inner: prost_build::Service,
+}
+
+impl DubboService {
+    fn new(inner: prost_build::Service) -> DubboService {
+        Self { inner }
+    }
+}
+
+impl super::Service for DubboService {
+    type Comment = String;
+
+    type Method = DubboMethod;
+
+    fn name(&self) -> &str {
+        &self.inner.name
+    }
+
+    fn package(&self) -> &str {
+        &self.inner.package
+    }
+
+    fn identifier(&self) -> &str {
+        &self.inner.proto_name
+    }
+
+    fn methods(&self) -> Vec<Self::Method> {
+        let mut ms = Vec::new();
+        for m in &self.inner.methods[..] {
+            ms.push(DubboMethod::new(Method {
+                name: m.name.clone(),
+                proto_name: m.proto_name.clone(),
+                comments: prost_build::Comments {
+                    leading_detached: m.comments.leading_detached.clone(),
+                    leading: m.comments.leading.clone(),
+                    trailing: m.comments.trailing.clone(),
+                },
+                input_type: m.input_type.clone(),
+                output_type: m.output_type.clone(),
+                input_proto_type: m.input_proto_type.clone(),
+                output_proto_type: m.output_proto_type.clone(),
+                options: m.options.clone(),
+                client_streaming: m.client_streaming,
+                server_streaming: m.server_streaming,
+            }))
+        }
+
+        ms
+    }
+
+    fn comment(&self) -> &[Self::Comment] {
+        &self.inner.comments.leading[..]
+    }
+}
+
+impl Clone for DubboService {
+    fn clone(&self) -> Self {
+        Self {
+            inner: prost_build::Service {
+                name: self.inner.name.clone(),
+                proto_name: self.inner.proto_name.clone(),
+                package: self.inner.package.clone(),
+                methods: {
+                    let mut ms = Vec::new();
+                    for m in &self.inner.methods[..] {
+                        ms.push(Method {
+                            name: m.name.clone(),
+                            proto_name: m.proto_name.clone(),
+                            comments: prost_build::Comments {
+                                leading_detached: m.comments.leading_detached.clone(),
+                                leading: m.comments.leading.clone(),
+                                trailing: m.comments.trailing.clone(),
+                            },
+                            input_type: m.input_type.clone(),
+                            output_type: m.output_type.clone(),
+                            input_proto_type: m.input_proto_type.clone(),
+                            output_proto_type: m.output_proto_type.clone(),
+                            options: m.options.clone(),
+                            client_streaming: m.client_streaming,
+                            server_streaming: m.server_streaming,
+                        })
+                    }
+
+                    ms
+                },
+                comments: prost_build::Comments {
+                    leading_detached: self.inner.comments.leading_detached.clone(),
+                    leading: self.inner.comments.leading.clone(),
+                    trailing: self.inner.comments.trailing.clone(),
+                },
+                options: self.inner.options.clone(),
+            },
+        }
+    }
+}
+
+pub struct DubboMethod {
+    inner: Method,
+}
+
+impl DubboMethod {
+    fn new(m: Method) -> DubboMethod {
+        Self { inner: m }
+    }
+}
+
+impl super::Method for DubboMethod {
+    type Comment = String;
+
+    fn name(&self) -> &str {
+        &self.inner.name
+    }
+
+    fn identifier(&self) -> &str {
+        &self.inner.proto_name
+    }
+
+    fn codec_path(&self) -> &str {
+        "triple::codec::serde_codec::SerdeCodec"
+    }
+
+    fn client_streaming(&self) -> bool {
+        self.inner.client_streaming
+    }
+
+    fn server_streaming(&self) -> bool {
+        self.inner.server_streaming
+    }
+
+    fn comment(&self) -> &[Self::Comment] {
+        &self.inner.comments.leading[..]
+    }
+
+    fn request_response_name(
+        &self,
+        proto_path: &str,
+        compile_well_known_types: bool,
+    ) -> (TokenStream, TokenStream) {
+        let convert_type = |proto_type: &str, rust_type: &str| -> TokenStream {
+            if (is_google_type(proto_type) && !compile_well_known_types)
+                || rust_type.starts_with("::")
+                || NON_PATH_TYPE_ALLOWLIST.iter().any(|t| *t == rust_type)
+            {
+                rust_type.parse::<TokenStream>().unwrap()
+            } else if rust_type.starts_with("crate::") {
+                syn::parse_str::<syn::Path>(rust_type)
+                    .unwrap()
+                    .to_token_stream()
+            } else {
+                syn::parse_str::<syn::Path>(&format!("{}::{}", proto_path, rust_type))
+                    .unwrap()
+                    .to_token_stream()
+            }
+        };
+
+        let req = convert_type(&self.inner.input_proto_type, &self.inner.input_type);
+        let resp = convert_type(&self.inner.output_proto_type, &self.inner.output_type);
+
+        (req, resp)
+    }
+}
+
+impl Clone for DubboMethod {
+    fn clone(&self) -> Self {
+        DubboMethod::new(Method {
+            name: self.inner.name.clone(),
+            proto_name: self.inner.proto_name.clone(),
+            comments: prost_build::Comments {
+                leading_detached: self.inner.comments.leading_detached.clone(),
+                leading: self.inner.comments.leading.clone(),
+                trailing: self.inner.comments.trailing.clone(),
+            },
+            input_type: self.inner.input_type.clone(),
+            output_type: self.inner.output_type.clone(),
+            input_proto_type: self.inner.input_proto_type.clone(),
+            output_proto_type: self.inner.output_proto_type.clone(),
+            options: self.inner.options.clone(),
+            client_streaming: self.inner.client_streaming,
+            server_streaming: self.inner.server_streaming,
+        })
+    }
+}
+
+/// Non-path Rust types allowed for request/response types.
+const NON_PATH_TYPE_ALLOWLIST: &[&str] = &["()"];
+
+fn is_google_type(proto_type: &str) -> bool {
+    proto_type.starts_with(".google.protobuf")
+}
