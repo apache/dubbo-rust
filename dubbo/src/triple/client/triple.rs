1776d99ecc7015e82879b7f1386d13417179f586
liuyuancheng <tenstone@foxmail.com>
2023-02-17 12:58:15 +0800

    feat: merged branch main to service_discovery

--- dubbo/src/triple/client/triple.rs
+++ dubbo/src/triple/client/triple.rs
@@ -151,8 +151,8 @@ impl TripleClient {
         .into_stream();
         let body = hyper::Body::wrap_stream(body_stream);
         let arc_invocation = Arc::new(invocation);
-        let mut req;
-        let mut http_uri;
+        let req;
+        let http_uri;
         if self.cluster_invoker.is_some() {
             let cluster_invoker = self.cluster_invoker.as_ref().unwrap().clone();
             req = cluster_invoker.build_req(self, path, arc_invocation.clone(), body);
@@ -224,17 +224,24 @@ impl TripleClient {
         .into_stream();
         let body = hyper::Body::wrap_stream(en);
 
-        let url_list = self
-            .directory
-            .as_ref()
-            .expect("msg")
-            .list(Arc::new(invocation));
-        let real_url = url_list.choose(&mut rand::thread_rng()).expect("msg");
-        let http_uri =
-            http::Uri::from_str(&format!("http://{}:{}/", real_url.ip, real_url.port)).unwrap();
-
-        let req = self.map_request(http_uri.clone(), path, body);
-
+        let arc_invocation = Arc::new(invocation);
+        let req;
+        let http_uri;
+        if self.cluster_invoker.is_some() {
+            let cluster_invoker = self.cluster_invoker.as_ref().unwrap().clone();
+            req = cluster_invoker.build_req(self, path, arc_invocation.clone(), body);
+            http_uri = req.uri().clone();
+        } else {
+            let url_list = self
+                .directory
+                .as_ref()
+                .expect("msg")
+                .list(arc_invocation.clone());
+            let real_url = url_list.choose(&mut rand::thread_rng()).expect("msg");
+            http_uri =
+                http::Uri::from_str(&format!("http://{}:{}/", real_url.ip, real_url.port)).unwrap();
+            req = self.map_request(http_uri.clone(), path, body);
+        }
         let mut conn = Connection::new().with_host(http_uri);
         let response = conn
             .call(req)
@@ -274,17 +281,24 @@ impl TripleClient {
         .into_stream();
         let body = hyper::Body::wrap_stream(en);
 
-        let url_list = self
-            .directory
-            .as_ref()
-            .expect("msg")
-            .list(Arc::new(invocation));
-        let real_url = url_list.choose(&mut rand::thread_rng()).expect("msg");
-        let http_uri =
-            http::Uri::from_str(&format!("http://{}:{}/", real_url.ip, real_url.port)).unwrap();
-
-        let req = self.map_request(http_uri.clone(), path, body);
-
+        let arc_invocation = Arc::new(invocation);
+        let req;
+        let http_uri;
+        if self.cluster_invoker.is_some() {
+            let cluster_invoker = self.cluster_invoker.as_ref().unwrap().clone();
+            req = cluster_invoker.build_req(self, path, arc_invocation.clone(), body);
+            http_uri = req.uri().clone();
+        } else {
+            let url_list = self
+                .directory
+                .as_ref()
+                .expect("msg")
+                .list(arc_invocation.clone());
+            let real_url = url_list.choose(&mut rand::thread_rng()).expect("msg");
+            http_uri =
+                http::Uri::from_str(&format!("http://{}:{}/", real_url.ip, real_url.port)).unwrap();
+            req = self.map_request(http_uri.clone(), path, body);
+        }
         let mut conn = Connection::new().with_host(http_uri);
         let response = conn
             .call(req)
@@ -339,17 +353,24 @@ impl TripleClient {
         )
         .into_stream();
         let body = hyper::Body::wrap_stream(en);
-
-        let url_list = self
-            .directory
-            .as_ref()
-            .expect("msg")
-            .list(Arc::new(invocation));
-        let real_url = url_list.choose(&mut rand::thread_rng()).expect("msg");
-        let http_uri =
-            http::Uri::from_str(&format!("http://{}:{}/", real_url.ip, real_url.port)).unwrap();
-
-        let req = self.map_request(http_uri.clone(), path, body);
+        let arc_invocation = Arc::new(invocation);
+        let req;
+        let http_uri;
+        if self.cluster_invoker.is_some() {
+            let cluster_invoker = self.cluster_invoker.as_ref().unwrap().clone();
+            req = cluster_invoker.build_req(self, path, arc_invocation.clone(), body);
+            http_uri = req.uri().clone();
+        } else {
+            let url_list = self
+                .directory
+                .as_ref()
+                .expect("msg")
+                .list(arc_invocation.clone());
+            let real_url = url_list.choose(&mut rand::thread_rng()).expect("msg");
+            http_uri =
+                http::Uri::from_str(&format!("http://{}:{}/", real_url.ip, real_url.port)).unwrap();
+            req = self.map_request(http_uri.clone(), path, body);
+        }
 
         let mut conn = Connection::new().with_host(http_uri);
         let response = conn
