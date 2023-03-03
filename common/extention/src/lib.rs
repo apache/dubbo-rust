6ad7aa24c547087197ab8bfffd02b10364a6c78d
liuyuancheng <tenstone@foxmail.com>
2023-03-03 16:28:36 +0800

    Ftr: add new-arc related packages

--- common/extention/src/lib.rs
+++ common/extention/src/lib.rs
@@ -14,12 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
+        let result = add(2, 2);
         assert_eq!(result, 4);
     }
 }
---
+++ protocol/README.md
@@ -0,0 +1,4 @@
+/protocol
+    /protocol # define protocol abstract layer
+    /dubbo2   # for dubbo2 protocol, hessian2 codec as default
+    /triple   # for triple protocol
