72270600b1f359ea96ab6bf6efe5feef333d82c1
baerwang <52104949+baerwang@users.noreply.github.com>
2023-04-11 08:32:24 +0800

    feat:tls (#104)
    Co-authored-by: wangxiaoxiong <wangxiaoxiong@asants.com>
    Co-authored-by: Yang Yang <962032265@qq.com>

--- dubbo/src/utils/tls.rs
+++ dubbo/src/utils/tls.rs
@@ -15,6 +15,22 @@
  * limitations under the License.
  */
 
-pub mod boxed;
-pub mod boxed_clone;
-pub mod tls;
+use rustls_pemfile::{certs, rsa_private_keys};
+use std::{
+    fs::File,
+    io::{self, BufReader},
+    path::Path,
+};
+use tokio_rustls::rustls::{Certificate, PrivateKey};
+
+pub fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
+    certs(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
+        .map(|mut certs| certs.drain(..).map(Certificate).collect())
+}
+
+pub fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
+    rsa_private_keys(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
+        .map(|mut keys| keys.drain(..).map(PrivateKey).collect())
+}
